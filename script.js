5899d93dd6e7d4251521afe3ffbef64fd679bedc
kiber-men <80974688+kiber-men@users.noreply.github.com>
2024-05-02 18:51:20 +0300

    header + works done

---
+++ script.js
@@ -0,0 +1,24 @@
+let buttons = document.querySelectorAll(".nav__button");
+console.log(buttons)
+window.onscroll = function() {
+    let h =document.documentElement.clientHeight;
+    let activeButton;
+    if (window.scrollY >= 4*h) {
+        activeButton = buttons[4];
+    }
+    else if (window.scrollY >= 3*h) {
+        activeButton = buttons[3];
+    }
+    else if (window.scrollY >= 2*h) {
+        activeButton = buttons[2];
+    }
+    else if (window.scrollY >= h) {
+        activeButton = buttons[1];
+    }
+    else {
+        activeButton = buttons[0];
+    }
+    let oldActive = document.querySelector(".active");
+    oldActive.classList.remove ("active")
+    activeButton.classList.add ("active")
+}
